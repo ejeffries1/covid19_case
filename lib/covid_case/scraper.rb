2d7d2ef8006278da5af8e0e21df23f545adf6295
evan <ejeffries14@gmail.com>
2021-08-12 19:29:16 -0400

    scraper update

--- lib/covid_case/scraper.rb
+++ lib/covid_case/scraper.rb
@@ -1,8 +1,20 @@
 class CovidCase::Scraper
     def self.main_page
         doc = Nokogiri::HTML(open("https://coronavirus.jhu.edu/region"))
-        doc.css(".RegionMenu_items__3D_d2").each do |state|
-            state
+        doc.css(".RegionMenu_items__3D_d2 a").collect do |state|
+            name = state.css("span")
+        end
+    end
+
+    def self.state_page
+        doc = Nokogiri::HTML(open("https://coronavirus.jhu.edu/region/us/alaska"))
+        doc.css(".RegionOverview_mainSections__3DQD7 RegionOverview_noBorder__1yP6L").collect do |attr|
+            {
+                :deaths =>
+                :cases =>
+                :tested =>
+                :vaccinated =>
+            }
             binding.pry
         end
     end
