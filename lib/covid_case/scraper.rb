232464ff8428db528609f5a4c6dfa33f7a24bc3a
evan <ejeffries14@gmail.com>
2021-08-14 19:32:07 -0400

    snew scraper website

--- lib/covid_case/scraper.rb
+++ lib/covid_case/scraper.rb
@@ -1,14 +1,14 @@
 class CovidCase::Scraper
     def self.main_page
-        doc = Nokogiri::HTML(open("https://coronavirus.jhu.edu/region"))
-        doc.css(".RegionMenu_items__3D_d2 a").collect do |state|
-            name = state.css("span")
+        doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/covid-cases.html"))
+        doc.css(".children a").collect do |state|
+            name = state.text
         end
     end
 
     def self.state_page
-        doc = Nokogiri::HTML(open("https://coronavirus.jhu.edu/region/us/alabama"))
-        doc.css(".RegionOverview_mainSections__3DQD7").collect do |element|
+        doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/louisiana-covid-cases.html"))
+        doc.css(".parent td").collect do |element|
             death = element.css(".OverviewBlock_statValue__1bVFH")
             #cases
             #tested
