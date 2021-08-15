570b105a4b88a7650c22cf5c3e04c3962269ac75
evan <ejeffries14@gmail.com>
2021-08-15 19:33:22 -0400

    CLI update

--- lib/covid_case/cli.rb
+++ lib/covid_case/cli.rb
@@ -1,5 +1,16 @@
 class CovidCase::CLI
-    def call
-        puts "Hello!!!"
+
+    BASE_PATH = "https://www.nytimes.com/interactive/2021/us/covid-cases.html"
+
+    puts "Welcome to the Covid-Case CLI"
+    puts "Please make a selection"
+
+    def run
+        make_state
     end
+
+    def make_state
+        students_array = Scraper.main_page(BASE_PATH)
+    end
+
 end
--- lib/covid_case/scraper.rb
+++ lib/covid_case/scraper.rb
@@ -1,19 +1,21 @@
 class CovidCase::Scraper
-    def self.main_page
-        doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/covid-cases.html"))
+    def self.main_page(main)
+        doc = Nokogiri::HTML(open(main))
         doc.css(".children a").collect do |state|
             name = state.text
+            binding.pry
         end
     end
 
     def self.state_page
         doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/louisiana-covid-cases.html"))
-        doc.css(".parent td").collect do |element|
-            death = element.css(".OverviewBlock_statValue__1bVFH")
-            #cases
-            #tested
-            #vaccinated
-            binding.pry
+        covid_hash = {}
+        doc.css(".covid-tracker").each do |element|
+            covid_hash[:death] = element.css(".num.deaths.svelte-6tbkhx")[2].text
+            covid_hash[:cases] = element.css(".num.cases.svelte-6tbkhx")[2].text
+            covid_hash[:vaccinated] = element.css(".num.vax.td-end")[0].text
+            #binding.pry
         end
+        covid_hash
     end
 end
