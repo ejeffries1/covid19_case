7bf9a19ef54aac043cf4386cfcdfd5a241b90f95
evan <ejeffries14@gmail.com>
2021-08-17 01:53:45 -0400

    scraper update

--- lib/covid_case/scraper.rb
+++ lib/covid_case/scraper.rb
@@ -1,22 +1,28 @@
 class CovidCase::Scraper
-    def self.main_page
+    attr_accessor
+
+    def self.scrape_main_page
+        students_info = []
         doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/covid-cases.html"))
-        doc.css("table.g-table.super-table.withchildren").collect do |state|
-            states = []
-                states << name = state.css(".name").text
-            binding.pry
+        doc.css("table.g-table.super-table.withchildren").each do |state|
+            state_info << {
+                :name => state.css("td.name").text,
+                :state_url => state.css("tbody.children a").attribute("href").value
+            }
         end
-        states
+        @state_info
     end
 
-    def self.state_page
-        doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/louisiana-covid-cases.html"))
+    def self.scrape_state_page
+        page = @state_info
+        binding.pry
+        doc = Nokogiri::HTML(open(page))
         covid_hash = {}
         doc.css(".covid-tracker").each do |element|
             covid_hash[:death] = element.css(".num.deaths.svelte-6tbkhx")[2].text
             covid_hash[:cases] = element.css(".num.cases.svelte-6tbkhx")[2].text
             covid_hash[:vaccinated] = element.css(".num.vax.td-end")[0].text
-            #binding.pry
+            
         end
         covid_hash
     end
