0dc134b6a6bdb2694b68739280f4d04156737bfb
evan <ejeffries14@gmail.com>
2021-08-17 02:50:34 -0400

    scraper update

--- lib/covid_case/scraper.rb
+++ lib/covid_case/scraper.rb
@@ -1,29 +1,29 @@
 class CovidCase::Scraper
-    attr_accessor
+    attr_accessor 
 
     def self.scrape_main_page
-        students_info = []
+        states_info = []
         doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/covid-cases.html"))
         doc.css("table.g-table.super-table.withchildren").each do |state|
-            state_info << {
-                :name => state.css("td.name").text,
-                :state_url => state.css("tbody.children a").attribute("href").value
-            }
+            #states_info << {
+                name = state.css(".children tr .name").text
+            
+                state_url = state.css("tbody.children a").attribute("href").value
+            #}
+            binding.pry
         end
-        @state_info
+        #states_info
     end
 
     def self.scrape_state_page
-        page = @state_info
-        binding.pry
-        doc = Nokogiri::HTML(open(page))
-        covid_hash = {}
-        doc.css(".covid-tracker").each do |element|
-            covid_hash[:death] = element.css(".num.deaths.svelte-6tbkhx")[2].text
-            covid_hash[:cases] = element.css(".num.cases.svelte-6tbkhx")[2].text
-            covid_hash[:vaccinated] = element.css(".num.vax.td-end")[0].text
-            
-        end
-        covid_hash
+        #doc = Nokogiri::HTML(open("https://www.nytimes.com/interactive/2021/us/covid-cases.html"))
+        #covid_hash = {}
+        #doc.css(".covid-tracker").each do |element|
+            #covid_hash[:death] = element.css(".num.deaths.svelte-6tbkhx")[2].text
+            #covid_hash[:cases] = element.css(".num.cases.svelte-6tbkhx")[2].text
+            #covid_hash[:vaccinated] = element.css(".num.vax.td-end")[0].text
+            #binding.pry
+        #end
+        #covid_hash
     end
 end
