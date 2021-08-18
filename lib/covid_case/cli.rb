1d211cde1f98daf7473923f85555b8b8c12d88f7
evan <ejeffries14@gmail.com>
2021-08-18 03:12:01 -0400

    cli connection

--- lib/covid_case/cli.rb
+++ lib/covid_case/cli.rb
@@ -1,16 +1,23 @@
 class CovidCase::CLI
 
-    BASE_PATH = "https://www.nytimes.com/interactive/2021/us/covid-cases.html"
+    #BASE_PATH = "https://www.nytimes.com/interactive/2021/us/covid-cases.html"
 
     def call
         puts "Welcome to the Covid-Case CLI"
         puts "Please make a selection"
-        #get_state
+        get_state
+        list_states
         #get_case(state)
     end
 
     def get_state
-        
+        @place = CovidCase::States.all
+    end
+
+    def list_states
+        @place.each.with_index(1) do |state, index|
+            puts "#{index}. #{state}"
+        end
     end
 
     def make_state
--- lib/covid_case/states.rb
+++ lib/covid_case/states.rb
@@ -1,14 +1,21 @@
 class CovidCase::States
-    attr_accessor :name
+    attr_accessor :name, :state
 
     @@all = []
     def initialize(name)
         @name = name
-        #binding.pry
+        #@state = states_info[:state_url]
+        save
+        binding.pry
+    end
+    
+    def self.all
+        CovidCase::Scraper.scrape_main_page if @@all.empty?
+        @@all
+        binding.pry
     end
 
-    #def self.all
-    #CovidCase::Scraper.scrape_main_page
-    #binding.pry
-    #end
+    def save
+        @@all << self
+    end
 end
